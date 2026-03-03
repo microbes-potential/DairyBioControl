a2bf4bfda1c88db53c4ad5bb1fb235dc370b85a0
Adeel Farooq <jhwanj9@gmail.com>
2026-03-03 12:08:25 -0500

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -1,2 +1,16 @@
 from dash import html
-def page_about(): return html.Div([html.H2("👩‍🔬 About Us"), html.P("Developed by LaPointe Research Group, University of Guelph.")])
+
+def page_about():
+    return html.Div([
+        html.H2("👩‍🔬 About Us"),
+        html.P("Developed by LaPointe Research Group, University of Guelph."),
+
+        html.H4("Principal Investigator"),
+        html.P("Gisèle LaPointe"),
+        html.P("Department Web site:"),
+        html.A(
+            "https://www.uoguelph.ca/foodscience/people/gisele-lapointe",
+            href="https://www.uoguelph.ca/foodscience/people/gisele-lapointe",
+            target="_blank"
+        )
+    ])
