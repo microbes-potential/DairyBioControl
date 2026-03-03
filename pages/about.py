131b434529aae651fd917c3abdeba97b67970d9f
Adeel Farooq <jhwanj9@gmail.com>
2026-03-03 10:13:29 -0500

    Fix typo in About Us section

--- pages/about.py
+++ pages/about.py
@@ -1,3 +1,3 @@
 from dash import html
-def page_about(): return html.Div([html.H2("👩‍🔬 About Us"), html.P("Developed by LaPointes Research Group, University of Guelph.")])
+def page_about(): return html.Div([html.H2("👩‍🔬 About Us"), html.P("Developed by LaPointe Research Group, University of Guelph.")])
 
